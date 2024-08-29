af44026bc1da9c2f2aeeeb8df69a463b88ed81c0
Vitaliy <Vitaliy@yandex.ru>
2024-08-29 12:54:32 +0300

    tg: подключение и настройка тг бота

--- core/apps.py
+++ core/apps.py
@@ -5,3 +5,7 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
     verbose_name = 'Записи клиентов'
+
+    # По готовности приложения будет импортирован сигнал
+    def ready(self):
+        import core.signals
