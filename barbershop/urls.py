2f95b2a7648c0f86dea0dcacaa8f97afa79dbfd5
Vitaliy <Vitaliy@yandex.ru>
2024-08-30 14:55:05 +0300

    static; подключили статику и медиа

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import main, thanks
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main),
     path('thanks/', thanks, name='thanks'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
