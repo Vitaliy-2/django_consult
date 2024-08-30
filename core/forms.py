dafa1147e6d44481e6075e56e15166bb6ae36ec8
Vitaliy <Vitaliy@yandex.ru>
2024-08-30 13:07:29 +0300

    forms: кастомизировали форму (если валидация не проходит)

--- core/forms.py
+++ core/forms.py
@@ -8,6 +8,15 @@ class VisitForm(forms.Form):
     phone = forms.CharField(label = 'Телефон', max_length=20, widget=forms.TextInput(attrs={'type': 'tel', 'placeholder': 'Номер телефона', 'class': 'form-control'}))
     comment = forms.CharField(label = 'Комментарий', required=False, widget=forms.Textarea(attrs={'placeholder': 'Комментарий', 'class': 'form-control'}))
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Если форма содержит ошибки, добавляем класс 'is-invalid' к соответствующим полям
+        # Поле с ошибкой приобретает красную рамку и воскл. знак
+        for field_name, field in self.fields.items():
+            if self.errors.get(field_name):
+                widget_classes = field.widget.attrs.get('class', '')
+                field.widget.attrs['class'] = f'{widget_classes} is-invalid'
+
     def clean_phone(self):
         # self.cleaned_data - словарь с данными, полученными из формы
         phone = self.cleaned_data.get("phone", '').strip()
