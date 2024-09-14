5f881592242e9316fa091ac9170abe5ad3d6c60e
Vitaliy <Vitaliy@yandex.ru>
2024-09-14 13:48:30 +0300

    Временная отработка logout через функцию

--- user/urls.py
+++ user/urls.py
@@ -7,11 +7,14 @@ from .views import login_view, logout_view, register_view
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html', next_page='main'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='main', http_method_names=['get', 'post'], 
-                                        template_name="logout.html"), name='logout'),
+    path('logout/', logout_view, name='logout'),
     path('register/', CreateView.as_view(
         template_name='register.html',
         form_class=UserCreationForm,
         success_url='/'
     ), name='register'),
 ]
+
+
+# path('logout/', LogoutView.as_view(next_page='main', http_method_names=['get', 'post'], template_name="logout.html"), name='logout'),
+# Пока что выход (logout) осуществляется через функцию во views
