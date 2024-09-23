a68c583c9c89127e9879c68c61b57c7dc29364b8
Vitaliy <Vitaliy@yandex.ru>
2024-09-23 12:20:23 +0300

    Восстановление пароля через email

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+
 from .views import (
     logout_view,
     CustomLoginView,
@@ -8,6 +12,7 @@ from .views import (
 )
 
 
+
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
@@ -15,8 +20,26 @@ urlpatterns = [
     path("password_change/", CustomPasswordChangeView.as_view(), name="password_change"),
     path("password_change/done/", CustomPasswordChangeDoneView.as_view(), name="password_change_done"),
     
+    # Маршруты восстановления пароля
+    # Маршрут для сброса пароля
+    path("password-reset/", auth_views.PasswordResetView.as_view(
+            template_name="password_reset_form.html",
+            email_template_name="password_reset_email.html",
+            success_url=reverse_lazy("password_reset_done"),),
+        name="password_reset",),
+
+    # Маршрут для подтверждения сброса пароля
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(
+            template_name="password_reset_done.html"), name="password_reset_done",),
+
+    # Маршрут для ввода нового пароля
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+            template_name="password_reset_confirm.html",
+            success_url=reverse_lazy("password_reset_complete"),),
+        name="password_reset_confirm",),
+
+    # Маршрут для завершения сброса пароля
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(
+            template_name="password_reset_complete.html"
+        ), name="password_reset_complete",),
 ]
-
-
-# path('logout/', LogoutView.as_view(next_page='main', http_method_names=['get', 'post'], template_name="logout.html"), name='logout'),
-# Пока что выход (logout) осуществляется через функцию во views
